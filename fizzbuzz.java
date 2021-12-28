b68da950f84d014be4121254a2887ba3547e6da5
octmoyo021 <octmoyo021@student.wethinkcode.co.za>
2021-12-28 08:31:49 +0200

    figured out how to get 'FIZZBUZZ' executed...I moved the if statement that checks for divisibility by 3 and 5 to the top so that it gets ran first

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -4,19 +4,20 @@ public class fizzbuzz {
         // STATEMENTS FOR A for loop SHOULD ALWAYS BE IN BRACKETS
         //i ++ ~ post-increament 
         for (int i = 1; i <= 50; i ++){
+
+            //CHECKING FOR DIVISIBILITY BY 3 AND 5 AND PRINT 'FIZZBUZZ'
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println("FIZZBUZZ");
+            }
+            
             //CHECKING FOR DIVISIBILITY BY 3 AND PRINT 'FIZZ'
-            if (i % 3 == 0){
+            else if (i % 3 == 0){
                 System.out.println("FIZZ");
             }
 
             //CHECKING FOR DIVISIBILTY BY 5 AND PRINT 'BUZZ'
             else if (i % 5 == 0){
                 System.out.println("BUZZ");
-            }
-
-            //CHECKING FOR DIVISIBILITY BY 3 AND 5 AND PRINT 'FIZZBUZZ'
-            else if (i % 3 == 0 && i % 5 == 0){
-                System.out.println("FIZZBUZZ");
             }
         }
     }
